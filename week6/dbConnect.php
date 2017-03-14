28ab3bc9906eb35cfe3fba105195c7ea6641123d
SirGeerald <1510605@rgu.ac.uk>
2017-03-14 17:10:46 +0000

    .

--- week6/dbConnect.php
+++ week6/dbConnect.php
@@ -1,38 +1,34 @@
-<?php
-$connectstr_dbhost='';
-$connectstr_dbname='';
-$connectstr_dbusername='';
-$connectstr_dbpassword='';
+<?PHP
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
 
-foreach ($_SERVER as $key => $value){
-
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0){
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
         continue;
     }
-
     $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
     $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
     $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?);.*$/", "\\1", $value);
-
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
 
-$link = mysqli_query($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
 
 define('DB_SERVER', $connectstr_dbhost);
-define('DB_USERNAME', $connectstr_dbusername);
-define('DB_PASSWORD', $connectstr_dbpassword);
-define('DB_DATABASE', $connectstr_dbname);
+define('DB_USERNAME', $connectstr_dbname);
+define('DB_PASSWORD', $connectstr_dbusername);
+define('DB_DATABASE', $connectstr_dbpassword);
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
-/*$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);*/
-
-if (!$link){
-    echo "Error Unable to Connect ZZ to MySQL." . PHP_EOL. "<br>";
-    echo "\n Debugging errno: " . mysqli_connect_errno(). PHP_EOL . "<br>";
-    echo "\n Debugging error: " . mysqli_connect_error(). PHP_EOL.  "<br>";
+if (!$link) {
+    echo "Error: Una ble to  connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
 
-
+?>
 
 
