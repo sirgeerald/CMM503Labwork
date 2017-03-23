b78f526e45b3b17b70352510ead5c13ca108a84c
SirGeerald <1510605@rgu.ac.uk>
2017-03-23 13:23:35 +0000

    .

--- week7a/index.php
+++ week7a/index.php
@@ -20,7 +20,7 @@
     <p>What will you like to do?</p>
 
     <ul>
-        <li><a href="insertSuperhero.php"> Insert a superhero</a></li>
+        <li><a href="superhero.html"> Insert a superhero</a></li>
         <li><a href="displaySuperheros.php"> Display all superheros</a></li>
         <li><a href="battle.php"> Insert a battle</a></li>
         <li><a href="displayBattles.php">Display all battles</a></li>
