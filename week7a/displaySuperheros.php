dfcd9d55932f233cbf9b2ed2012a801718bd2a6f
SirGeerald <1510605@rgu.ac.uk>
2017-03-23 13:45:00 +0000

    .

--- week7a/displaySuperheros.php
+++ week7a/displaySuperheros.php
@@ -26,8 +26,8 @@
     $result = $link->query($sql_query);
 
     while ($row = $result->fetch_array()) {
-        $firstname = $row['firstname'];
-        $lastname = $row['lastname'];
+        $firstname = $row['firstName'];
+        $lastname = $row['lastName'];
         $mainSuperpower = $row['mainSuperpower'];
         echo "<article>
             <h3> {$firstname} {$lastname} </h3>
