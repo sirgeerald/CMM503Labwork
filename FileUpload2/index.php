e029f6dfac4dde5daf0aefc7ec08518ce6842db1
SirGeerald <1510605@rgu.ac.uk>
2017-04-21 06:01:09 +0100

    .

---
+++ FileUpload2/index.php
@@ -0,0 +1,19 @@
+<?php
+?>
+
+
+<html>
+<head>
+    <title>Upload File Using PHP and Save in Directory - AllPHPTricks.com</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+</head>
+<body>
+<br /><br />
+<form name="form" method="post" action="upload.php" enctype="multipart/form-data" >
+    <input type="file" name="my_file" /><br /><br />
+    <input type="submit" name="submit" value="Upload"/>
+</form>
+<br /><br />
+
+</body>
+</html>
