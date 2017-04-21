b42c6d185db94c2c99977fc521a5d8d43c362d91
SirGeerald <1510605@rgu.ac.uk>
2017-04-21 07:33:02 +0100

    .

--- FileUpload/index.php
+++ FileUpload/index.php
@@ -1,36 +1,26 @@
 <?php
 include ('../registration/dbConnect.php');
-if(isset($_FILES['file_upload']))
+
+if(isset($_POST['btn_upload']))
 {
     session_start();
-    $userID = $_SESSION['username'];
+
     $name = $_FILES['file_upload']['name'];
     $size = $_FILES['file_upload']['size'];
     $tmp = $_FILES['file_upload']['tmp_name'];
     $type = $_FILES['file_upload']['type'];
 
-    $folder = 'uploads/'. $_FILES['file_upload']['name'];
 
-    $moved = move_uploaded_file($tmp, $folder);
+    if ($name){
 
-    if ($moved){
-        $add = $link->prepare("insert into tbl_uploads values(?,?,?)");
-        $add ->bind_param($name,$tmp,$size);
-        if ($add -> execute())
-        {
-            ?>
-            <script> alert(" Upload successful!")</script>
+        $location = "uploads/$name";
+        move_uploaded_file($tmp, $location);
 
-        <?php
-        }
-        else{
-            ?>
-            <script> alert(" Upload Failed!")</script>
-<?php
-        }
+        $link -> query("insert into tbl_uploads (file,type,size) VALUES('".$location."','".$type."','".$size."')");
+        header('location: index.php');
 
     }else{
-
+        die("Please select a file ");
     }
 
 }
