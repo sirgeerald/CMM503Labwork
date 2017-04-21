610abb715d04b71f491ac26a863dd9e38f527d3b
SirGeerald <1510605@rgu.ac.uk>
2017-04-21 08:23:15 +0100

    .

--- FileUpload/upload.php
+++ FileUpload/upload.php
@@ -6,6 +6,26 @@ include ('../registration/dbConnect.php');
 
     print_r($_FILES);
 
+    $targetPath = "uploads/";
+
+    $targetPath = $targetPath. basename($_FILES['file_upload']['name']);
+
+    if ($_FILES['file_upload']['size'] < 5000){
+        if (substr($_FILES['file_upload']['name'], -3) == 'txt'){
+            if (move_uploaded_file($_FILES['file_upload']['tmp_name'], $targetPath)){
+
+                echo "The file". basename($_FILES['file_upload']['name']). "has been uploaded";
+            }else{
+                echo "Error uploading file";
+            }
+        }else{
+            echo "You must upload a txt file";
+
+        }
+    }else{
+        echo "file too big";
+    }
+
     /*$name = $_FILES['file_upload']['name'];
     $size = $_FILES['file_upload']['size'];
     $tmp = $_FILES['file_upload']['tmp_name'];
